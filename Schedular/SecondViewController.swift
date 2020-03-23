d90e68138e7f68a2f5d4a61513df69188e5fe9d5
Kamaal Farah <kamaal.f1@gmail.com>
2020-03-23 18:42:57 +0100

    Creating a tab bar view

--- Schedular/SecondViewController.swift
+++ Schedular/SecondViewController.swift
@@ -12,7 +12,8 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("second view controller")
+        self.view.backgroundColor = .cyan
     }
 
 
